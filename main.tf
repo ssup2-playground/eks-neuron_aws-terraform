44b734db4b524d4af0dd79d7a42ebf62927078a8
ssup2 <supsup5642@gmail.com>
2024-09-23 01:57:23 +0900

    update

--- main.tf
+++ main.tf
@@ -149,6 +149,15 @@ module "eks" {
       type        = "ingress"
       self        = true
     }
+
+   ingress_grafana = {
+      description              = "From grafana NLB"
+      protocol                 = "-1"
+      from_port                = 0
+      to_port                  = 0
+      type                     = "ingress"
+      source_security_group_id = module.sg_grafana.security_group_id
+    }
   }
 }
 
@@ -310,3 +319,107 @@ resource "kubectl_manifest" "neuron_problem_detector" {
     helm_release.karpenter
   ]
 }
+
+## EKS / Load Balancer Controller
+module "irsa_load_balancer_controller" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name                              = format("%s-irsa-aws-load-balancer-controller", local.name)
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+resource "helm_release" "aws_load_balancer_controller" {
+  namespace  = "kube-system"
+  name       = "aws-load-balancer-controller"
+  chart      = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  version    = "v1.6.2"
+
+  values = [
+    templatefile("${path.module}/helm-values/aws-load-balancer-controller.yaml",
+      {
+        vpc_id                     = module.vpc.vpc_id
+        eks_cluster_name           = module.eks.cluster_name
+        controller_service_account = "aws-load-balancer-controller"
+        controller_iam_role        = module.irsa_load_balancer_controller.iam_role_arn
+      }
+    )
+  ]
+
+  depends_on = [
+    module.karpenter,
+    helm_release.karpenter,
+    module.irsa_load_balancer_controller
+  ]
+}
+
+## EKS / Prometheus
+resource "helm_release" "prometheus" {
+  namespace        = "observability"
+  create_namespace = true
+
+  name       = "prometheus"
+  chart      = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  version    = "25.27.0"
+
+  values = [
+    file("${path.module}/helm-values/prometheus.yaml")
+  ]
+
+  depends_on = [
+    module.karpenter,
+    helm_release.karpenter
+  ]
+}
+
+## EKS / Grafana
+module "sg_grafana" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name   = format("%s-grafana-sg", local.name)
+  vpc_id = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = "127.0.0.1/32"
+    }
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      from_port       = 0
+      to_port         = 0
+      protocol        = "-1"
+      cidr_blocks     = "0.0.0.0/0"
+    }
+  ]
+}
+
+resource "helm_release" "grafana" {
+  namespace        = "observability"
+  create_namespace = true
+
+  name       = "grafana"
+  chart      = "grafana"
+  repository = "https://grafana.github.io/helm-charts"
+  version    = "v7.0.8"
+
+  values = [
+    templatefile("${path.module}/helm-values/grafana.yaml", {
+      svc_sg = module.sg_grafana.security_group_id
+    })
+  ]
+}
+
+
